079b0b18c7f2ab5b6c420cdca7928f4a01dbde13
Your Name <you@example.com>
2020-04-20 19:37:24 -0300

    Updates

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,6 +1,34 @@
 'use strict';
 
 var path = require('path');
+// const through = require('through2');
+// const minify = require('gulp-minify');
+
+// function base64Encode(){
+//   return through.obj(function (vinylFile, encoding, callback) {
+//       // 1. clone new vinyl file for manipulation
+//       // (See https://github.com/wearefractal/vinyl for vinyl attributes and functions)
+//       var transformedFile = vinylFile.clone();
+  
+//       var filename = path.parse(transformedFile.path).name;
+
+//       // 2. set new contents
+//       // * contents can only be a Buffer, Stream, or null
+//       // * This allows us to modify the vinyl file in memory and prevents the need to write back to the file system.
+//       //transformedFile.contents = Buffer.from(`"${filename}": "${vinylFile.contents.toString('base64')}",`);
+//       transformedFile.contents = Buffer.from(`window.paletools = window.paletools || {};\nwindow.paletools['${filename}'] = "${vinylFile.contents.toString('base64')}";`);
+  
+//       // 3. pass along transformed file for use in next `pipe()`
+//       callback(null, transformedFile);
+//     });
+// }
+
+// gulp.task('mistictool', function () {
+//   return gulp.src('./src/mistictool/*.js')
+//           .pipe(minify())
+//           .pipe(base64Encode())
+//           .pipe(gulp.dest('./src/mistictool/dist/'));
+// });
 
 module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) {
   var dirs = config.directories;
@@ -37,6 +65,11 @@ module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) 
         path.join(dirs.temporary, '**/*'),
         '!' + path.join(dirs.temporary, '**/*.{css,map,html,js}')
       ]).on('change', browserSync.reload);
+
+      // gulp.watch([
+      //   path.join(dirs.temporary, '**/*'),
+      //   '!' + path.join(dirs.temporary, '**/*.{css,map,html,js}')
+      // ]).on('change', mistictool);
     }
   });
 };
